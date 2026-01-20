d54172c9c54a2224fae738bae6491b77439599f7
Tobias Bechtold <tobi.bechtold@gmail.com>
2026-01-20 20:55:09 +0000

    Changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "@/assets/logo-hero.png";
+import logo from "@/assets/logo-hero.webp";
 import LanguageSwitcher from "./LanguageSwitcher";
 import { useLanguage } from "@/i18n/LanguageContext";
 
